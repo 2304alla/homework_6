7e5594c5d05cb47a45fcae73b5a30c121589219c
allakoval <2448572@gmail.com>
2022-11-27 22:19:26 +0500

    dz

---
+++ seminar46/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+// 0  1  2  3   index
+
+// 1  4  8  19    0
+// 5 -2  33 -2    1
+// 77 3  8  1     2
+int[,] CreateMatrix(int rows, int columns,int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}| ");
+            else Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+
+int[,] arraY2D = CreateMatrix(3, 4, -100, 100);
+PrintMatrix(arraY2D);
---
+++ semunar49/Program.cs
@@ -0,0 +1,55 @@
+﻿// Решение в группах задач:
+// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}| ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+void ChangeMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] arraY2D = CreateMatrixSumIndex(3, 4);
+PrintMatrix(arraY2D);
+Console.WriteLine();
+
+ChangeMatrix(arraY2D);
+PrintMatrix(arraY2D);
